500fb810c3b90444b11ce7b3feaeabb6681d9944
Rishabhraj2754 <rishabhraj2754@gmail.com>
2022-12-22 16:51:24 +0530

    Adding: UC1 Employee present or absent

--- UCProblems.java
+++ UCProblems.java
@@ -1,5 +1,15 @@
 public class UCProblems {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee");
+        final int WAGE_PER_HR = 20;
+        final int FUL_DAY  = 8;
+        int pay = 0;
+        double num = Math.floor((Math.random()*10)) % 2;
+        if (num==1){
+            System.out.println("Employee is Present");
+        }
+        else{
+            System.out.println("Employee is Absent");
+        }
     }
 }
