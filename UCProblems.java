12804949e93a56b3cc72a5c09d588d8761cebbfb
Rishabhraj2754 <rishabhraj2754@gmail.com>
2022-12-22 17:27:04 +0530

    Adding: UC2 Daily Employee wage

--- UCProblems.java
+++ UCProblems.java
@@ -1,15 +1,18 @@
 public class UCProblems {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee");
-        final int WAGE_PER_HR = 20;
-        final int FUL_DAY  = 8;
-        int pay = 0;
+        System.out.println("Welcome to Employee wage computation program");
+        int DailyWAGE_PER_HR = 20;
+        int Emp_Hr  = 8;
+        int DailyWages = 0;
+        int IS_FULL_TIME = 1;
         double num = Math.floor((Math.random()*10)) % 2;
-        if (num==1){
+        if (IS_FULL_TIME == 1){
             System.out.println("Employee is Present");
+            DailyWages = Emp_Hr * DailyWAGE_PER_HR;
         }
         else{
             System.out.println("Employee is Absent");
         }
+        System.out.println("The daliy wages of employee is"+" "+DailyWages);
     }
 }
