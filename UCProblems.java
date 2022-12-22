ff324f1dae5e1a449c59381ed6f6d0b923f491f3
Rishabhraj2754 <rishabhraj2754@gmail.com>
2022-12-22 17:49:50 +0530

    Adding: Paet time in Employee Wage Problem

--- UCProblems.java
+++ UCProblems.java
@@ -1,18 +1,22 @@
 public class UCProblems {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation program");
-        int DailyWAGE_PER_HR = 20;
-        int Emp_Hr  = 8;
-        int DailyWages = 0;
-        int IS_FULL_TIME = 1;
-        double num = Math.floor((Math.random()*10)) % 2;
-        if (IS_FULL_TIME == 1){
+        int WAGE_PER_HR = 20;
+        int FULL_DAY= 8;
+        int PAY = 0;
+        int PART_TIME = 4;
+        double num = Math.floor((Math.random()*10)) % 3;
+        if (num == 1){
             System.out.println("Employee is Present");
-            DailyWages = Emp_Hr * DailyWAGE_PER_HR;
+            PAY = WAGE_PER_HR * FULL_DAY;
         }
-        else{
+        else if (num ==2){
+            System.out.println("Employee is PartTime");
+            PAY = WAGE_PER_HR * PART_TIME;
+        }
+        else if (num == 0){
             System.out.println("Employee is Absent");
         }
-        System.out.println("The daliy wages of employee is"+" "+DailyWages);
+        System.out.println("Employee paid per day is"+" "+PAY);
     }
 }
